86cf534e8743b6fd2ee3ec2a843e4370c1601f3e
grahaaluminium <grahaaluminiumperkasa@gmail.com>
2025-03-02 15:28:39 +0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -39,8 +39,8 @@ if data_source in ['stooq', 'yahooFinance']:
 else:
     saham_options = []
 
-if 'saham' not in st.session_state:
-    st.session_state.tickers = []
+# if 'saham' not in st.session_state:
+#     st.session_state.tickers = []
 # if select_mode == 'Random Select':
 #     st.session_state.tickers = random.sample(saham_options, 30)
 #     saham = st.multiselect("Select Stock:", options=saham_options, default=st.session_state.tickers)
